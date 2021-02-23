f40400735de737cb75c22213cd119a726b1f0729
yuukiasuna00 <yuukixasuna00@gmail.com>
2021-02-23 11:15:40 -0300

    feat: Add Profile component and experiencebar css

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/yuukiasuna00.png" alt="Yuuki Asuna" />
+      <div>
+        <strong>Yuuki Asuna</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
 
 export default function Home() {
   return (
     <div className="container">
       <ExperienceBar />
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div></div>
+      </section>
     </div>
   );
 }
