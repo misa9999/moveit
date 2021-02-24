7cb5e7737e698e78919d23f41a9c083b088331b4
yuukiasuna00 <yuukixasuna00@gmail.com>
2021-02-24 16:57:08 -0300

    fix: change challengeBoxNotActive to challengeNotActive

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,15 +1,21 @@
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
+  const hasActiveChallenge = true;
+
   return (
     <div className={styles.challengeBoxContainer}>
-      <div className={styles.challengeBoxNotActive}>
-        <strong>Start a cycle to receive challenges to be completed</strong>
-        <p>
-          <img src="icons/level-up.svg" alt="Level Up" />
-          Level up by completing challenges
-        </p>
-      </div>
+      {hasActiveChallenge ? (
+        <div />
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>Start a cycle to receive challenges to be completed</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up" />
+            Level up by completing challenges
+          </p>
+        </div>
+      )}
     </div>
   );
 }
